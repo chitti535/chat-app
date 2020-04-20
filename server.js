00627ff6312e2778bcb207d92591b1daff61c2aa
Metta <Chittibabu.Metta@chartindustries.com>
2020-04-20 18:54:30 +0530

    check

--- server.js
+++ server.js
@@ -13,15 +13,6 @@ io.on('connection', (socket)=> {
     console.log('user connected');
 });
 
- // Redirect all the other resquests
- this.app.get('*', (req, res) => {
-  if (allowedExt.filter(ext => req.url.indexOf(ext) > 0).length > 0) {
-    res.sendFile(path.resolve(`dist/${req.url}`));
-  } else {
-    res.sendFile(path.resolve('dist/index.html'));
-  }
-});
-
 server.listen(port, ()=>{
     console.log(`started on port: ${port}`);
 });
